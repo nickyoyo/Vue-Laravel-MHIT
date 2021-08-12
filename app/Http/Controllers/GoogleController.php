18b9e920035d3298938a6084c85bafc8500930b4
nick <nick@home33.com.tw>
2021-08-12 15:18:54 +0800

    2021.8.12-L+V-google

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -35,9 +35,7 @@ class GoogleController extends Controller
             $doc = DB::table('EM13')->where('E_MAIL',$user->email)->where('OFDT','00000000')->first();
             if(strpos($user->email,'@home33.com.tw')){            //是公司信箱                 
                 $finduser = User::where('google_id', $user->id)->first();
-                Session::put('EMID', $doc->EMID);
-                $EMID = Session::get('EMID');
-                dd($EMID);    
+                Session::put('EMID', $doc->EMID); 
                 if($finduser){ 
                     return redirect('/');
                 }else{
