c38be02faf9f498efdb08c3688370ebabc538ab0
nick <nick@home33.com.tw>
2021-08-13 14:22:18 +0800

    2021.8.13-L+V-快捷

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@
 import { createApp } from 'vue';
 import {createRouter, createWebHistory} from 'vue-router'
 import store from './store';
+import key from 'keymaster'
 //--------------- END ALL LIBRARY MODULES ------------------//
 
 // Routes
@@ -33,3 +34,11 @@ app.mount('#app')
 
 // Done.....!
 
+
+key('a', function(){ alert('you pressed a!') });
+
+// 回调函数返回 false 以阻止浏览器默认事件行为
+key('ctrl+r', function(){ alert('stopped reload!'); return false });
+
+// 绑定多个快捷键，做同一件事
+key('⌘+r, ctrl+r', function(){ });
--- resources/js/views/pages/CMsendSearch.vue
+++ resources/js/views/pages/CMsendSearch.vue
@@ -2,7 +2,7 @@
   <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
       <h1>查詢客戶資料</h1>
     <label for="PID">客戶編號:</label><br />
-    <input type="text" v-model="PID" /><br /><br />
+    <input type="text" v-model="PID" id="PID" /><br /><br />
     <button @click.prevent="searchCM" type="submit" >查詢資料</button>&nbsp;
     <button> <router-link to="/CM/接待自評" style="color:black;">新增客戶資料</router-link></button>
     <br />
